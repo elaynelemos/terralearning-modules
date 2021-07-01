dafcf3e8c37a1c68e6c884feeda05a7fa38e5563
elaynelemos <elayne.l.lemos@gmail.com>
2021-07-01 19:27:01 -0300

    feat: adds example to cluster

---
+++ services/webserver-cluster/examples/basic-example/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "webserver_cluster" {
+  source = "../../"
+
+  cluster_name  = "app-cluster-example"
+  instance_type = "t2.micro"
+  min_size      = 1
+  max_size      = 2
+}
