dbdb502fd9aa7e3e14995759805911f1292eb6e5
elaynelemos <elayne.l.lemos@gmail.com>
2021-07-02 11:49:12 -0300

    feat: adds iam usage example

---
+++ security/iam/examples/basic-example/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "iam_users" {
+  source = "../../"
+
+  user_names  = ["elayne", "terralearning"]
+}
---
+++ security/iam/examples/basic-example/outputs.tf
@@ -0,0 +1,9 @@
+output "all_users" {
+  value       = module.iam_users.all_users
+  description = "All IAM users."
+}
+
+output "all_arns" {
+  value = module.iam_users.all_arns
+  description = "The ARNs for all users."
+}
