fc1593b74df2136b9aac13f799dd54a2bfcd15ea
elaynelemos <elayne.l.lemos@gmail.com>
2021-07-02 12:32:57 -0300

    feat: adds some outputs

--- security/iam/outputs.tf
+++ security/iam/outputs.tf
@@ -7,3 +7,13 @@ output "all_arns" {
   value       = values(aws_iam_user.example)[*].arn
   description = "The ARNs for all users."
 }
+
+output "upper_names" {
+  value       = [for name in var.user_names: upper(name)]
+  description = "The user names uppercased."
+}
+
+output "short_upper_names" {
+  value       = [for name in var.user_names: upper(name) if length(name) < 7]
+  description = "The user names uppercased."
+}
