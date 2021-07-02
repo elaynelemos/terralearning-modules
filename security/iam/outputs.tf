f7e72ae1262c17e2cbcff2669dfb47ca3bec20a1
elaynelemos <elayne.l.lemos@gmail.com>
2021-07-02 12:34:43 -0300

    fix: syntax

--- security/iam/outputs.tf
+++ security/iam/outputs.tf
@@ -9,11 +9,11 @@ output "all_arns" {
 }
 
 output "upper_names" {
-  value       = [for name in var.user_names: upper(name)]
+  value       = [for name in var.user_names : upper(name)]
   description = "The user names uppercased."
 }
 
 output "short_upper_names" {
-  value       = [for name in var.user_names: upper(name) if length(name) < 7]
+  value       = [for name in var.user_names : upper(name) if length(name) < 7]
   description = "The user names uppercased."
 }
